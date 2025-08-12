35f0169b446e4dc280878d2455f194f8ddf29b68
Animesh Dhakal <dhakalanimesh385@gmail.com>
2025-08-12 08:32:16 +0545

    Added new approach

---
+++ app.py
@@ -0,0 +1,118 @@
+import asyncio
+import os
+from contextlib import asynccontextmanager
+from uuid import uuid4
+
+import zendriver as zd
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+
+load_dotenv()
+
+browser = None
+
+
+class URLRequest(BaseModel):
+    url: str
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global browser
+    browser = await zd.start(user_data_dir=os.getenv("USER_DATA_DIR"))
+    yield
+    if browser:
+        await browser.stop()
+
+
+async def wait_for_page_load(tab: zd.Tab) -> bool:
+    wait_fut = tab.evaluate(
+        expression="""
+        new Promise((resolve) => {
+            const checkReady = () => {
+                if (document.readyState === 'complete' && document.body) {
+                    resolve(true);
+                } else {
+                    setTimeout(checkReady, 100);
+                }
+            };
+            checkReady();
+        });
+        """,
+        await_promise=True,
+    )
+    try:
+        await asyncio.wait_for(wait_fut, timeout=15)
+        return True
+    except asyncio.TimeoutError:
+        return False
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.post("/extract")
+async def extract(request: URLRequest):
+    url = request.url
+    tab = None
+
+    try:
+        tab = await browser.get(url, new_tab=True)
+
+        await asyncio.sleep(1)
+
+        await wait_for_page_load(tab)
+
+        await asyncio.sleep(1)
+
+        source_code = await tab.evaluate("document.documentElement.outerHTML")
+
+        return source_code
+
+    except Exception:
+        return ""
+
+    finally:
+        if tab:
+            await tab.close()
+
+
+@app.post("/screenshot")
+async def screenshot(request: URLRequest):
+    url = request.url
+    tab = None
+
+    try:
+        tab = await browser.get(url, new_tab=True)
+
+        await asyncio.sleep(1)
+
+        await wait_for_page_load(tab)
+
+        await asyncio.sleep(1)
+
+        file_name = f"screenshot_{str(uuid4()).split('-')[0]}.png"
+
+        await tab.save_screenshot(f"screenshots/{file_name}")
+
+        return {"message": "Screenshot saved", "path": f"{file_name}"}
+
+    except Exception:
+        return ""
+
+    finally:
+        if tab:
+            await tab.close()
+
+
+@app.get("/screenshots/{file_name}")
+async def screenshots(file_name: str):
+    return FileResponse(f"screenshots/{file_name}")
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=9999)
